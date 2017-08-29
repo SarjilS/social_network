07595b1b204a125f54fb0825b18fef36ffedd688
Caleb Mazalevskis <maikuolan@gmail.com>
2017-08-29 23:54:16 +0800

    Removes excess whitespace.
    Doesn't affect the actual code at all, but makes everything just look a
    little cleaner for other coders, developers, etc.

--- chat.php
+++ chat.php
@@ -2,46 +2,46 @@
 
 class Chat extends CI_Controller {
 
-	public function send_message()
-	{
-		$reciver = $_POST['receiver'];
-		$msg = htmlspecialchars($this->input->post('msg'));
-		$sender = $this->session->userdata('user');
-		
-		$this->load->model('chat_model');
-		if($this->chat_model->send_message($sender, $reciver, $msg))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public function send_message()
+    {
+        $reciver = $_POST['receiver'];
+        $msg = htmlspecialchars($this->input->post('msg'));
+        $sender = $this->session->userdata('user');
 
-	public function get_message()
-	{
-		$reciver = $_POST['fid'];
-		$sender = $_POST['mid'];
-		
-		$this->load->model('chat_model');
-		if($msg_data = $this->chat_model->get_message($sender, $reciver))
-		{
-			$data = array(
-				'message'=> $msg_data->message,
-				'time' => $msg_data->time, 
-				'read'=> $msg_data->read
-			 );
-			 $msg_id = $msg_data->id;
-			 $this->chat_model->read_message($msg_id);
-		}
-		else
-		{
-			return false;
-		}
+        $this->load->model('chat_model');
+        if($this->chat_model->send_message($sender, $reciver, $msg))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
-		echo json_encode($data);
-	}
+    public function get_message()
+    {
+        $reciver = $_POST['fid'];
+        $sender = $_POST['mid'];
+
+        $this->load->model('chat_model');
+        if($msg_data = $this->chat_model->get_message($sender, $reciver))
+        {
+            $data = array(
+                'message'=> $msg_data->message,
+                'time' => $msg_data->time,
+                'read'=> $msg_data->read
+             );
+             $msg_id = $msg_data->id;
+             $this->chat_model->read_message($msg_id);
+        }
+        else
+        {
+            return false;
+        }
+
+        echo json_encode($data);
+    }
 }
 
 /* End of file chat.php */
--- chat_model.php
+++ chat_model.php
@@ -2,59 +2,59 @@
 
 class chat_model extends CI_Model
 {
-	public function send_message($sender, $reciver, $msg)
-	{
-		$this->load->database();
-		$this->db->select('u_id');
-		$this->db->where('user_name', $sender);
-		$query = $this->db->get('users');
-		$sender_id = $query->row()->u_id;
-		
-		$time = date("d/m/y")." ".date("h:i a");
-		$message_data= array(
-			'to' 	  => $reciver,
-			'from'    => $sender_id,
-			'message' => $msg,
-			'time' 	  => $time
-		);
-		
-		$this->db->insert('chat', $message_data);
-	}
-	
-	/*
-	public function is_received($sender, $reciver)
-	{
-		$this->load->database();
-		$this->db->select('id');
-		$this->db->where('to', $reciver);
-		$this->db->where('from', $sender);
-		$this->db->where('read', 0);
-		$query = $this->db->get('chat');
-		
-		return $query->num_rows();
-	}
-	*/
-	
-	public function get_message($sender, $reciver)
-	{
-		$this->load->database();
-		$this->db->where('to', $reciver);
-		$this->db->where('from', $sender);
-		$this->db->where('read', 0);
-		$this->db->order_by('id', 'DESC');
-		$this->db->limit('1');
-		$query = $this->db->get('chat');
-		
-		return $query->row();
-	}
-	
-	public function read_message($msg_id)
-	{
-		$this->load->database();
-		$msg = array('read' => '1');
-		$this->db->where('id', $msg_id);
-		$this->db->update('chat', $msg);
-		return true;
-	}
+    public function send_message($sender, $reciver, $msg)
+    {
+        $this->load->database();
+        $this->db->select('u_id');
+        $this->db->where('user_name', $sender);
+        $query = $this->db->get('users');
+        $sender_id = $query->row()->u_id;
+
+        $time = date("d/m/y")." ".date("h:i a");
+        $message_data= array(
+            'to'       => $reciver,
+            'from'    => $sender_id,
+            'message' => $msg,
+            'time'       => $time
+        );
+
+        $this->db->insert('chat', $message_data);
+    }
+
+    /*
+    public function is_received($sender, $reciver)
+    {
+        $this->load->database();
+        $this->db->select('id');
+        $this->db->where('to', $reciver);
+        $this->db->where('from', $sender);
+        $this->db->where('read', 0);
+        $query = $this->db->get('chat');
+
+        return $query->num_rows();
+    }
+    */
+
+    public function get_message($sender, $reciver)
+    {
+        $this->load->database();
+        $this->db->where('to', $reciver);
+        $this->db->where('from', $sender);
+        $this->db->where('read', 0);
+        $this->db->order_by('id', 'DESC');
+        $this->db->limit('1');
+        $query = $this->db->get('chat');
+
+        return $query->row();
+    }
+
+    public function read_message($msg_id)
+    {
+        $this->load->database();
+        $msg = array('read' => '1');
+        $this->db->where('id', $msg_id);
+        $this->db->update('chat', $msg);
+        return true;
+    }
 }
 ?>
