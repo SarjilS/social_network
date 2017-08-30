3b48b4c406ec62fe541f9f90d045efbf7d58ac20
Sarjil Shaikh <SarjilS@users.noreply.github.com>
2017-08-30 12:26:58 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -1,32 +1,27 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
+if ( ! defined('BASEPATH')) {
+	exit('No direct script access allowed');
+}
 
-class Chat extends CI_Controller {
-
+class Chat extends CI_Controller
+{
 	public function send_message()
 	{
-		$reciver = $_POST['receiver'];
+		$receiver = $_POST['receiver'];
 		$msg = htmlspecialchars($this->input->post('msg'));
 		$sender = $this->session->userdata('user');
 		
 		$this->load->model('chat_model');
-		if($this->chat_model->send_message($sender, $reciver, $msg))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return ($this->chat_model->send_message($sender, $receiver, $msg)) ? true : false;
 	}
 
 	public function get_message()
 	{
-		$reciver = $_POST['fid'];
+		$receiver = $_POST['fid'];
 		$sender = $_POST['mid'];
 		
 		$this->load->model('chat_model');
-		if($msg_data = $this->chat_model->get_message($sender, $reciver))
-		{
+		if($msg_data = $this->chat_model->get_message($sender, $receiver)) {
 			$data = array(
 				'message'=> $msg_data->message,
 				'time' => $msg_data->time, 
@@ -34,15 +29,14 @@ class Chat extends CI_Controller {
 			 );
 			 $msg_id = $msg_data->id;
 			 $this->chat_model->read_message($msg_id);
-		}
-		else
+		} else
 		{
 			return false;
 		}
-
 		echo json_encode($data);
 	}
 }
 
 /* End of file chat.php */
+
 /* Location: ./application/controllers/chat.php */
